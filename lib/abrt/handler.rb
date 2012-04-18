f653559ddc22a36b3d30b6fe40111996996634c3
Michal Fojtik <mfojtik@redhat.com>
2012-04-18 14:19:30 +0200

    Removed begin/rescue block

--- lib/abrt/handler.rb
+++ lib/abrt/handler.rb
@@ -1,35 +1,34 @@
 module ABRT
+  require 'abrt/exception'
+  require 'socket'
+  require 'syslog'
+
+  # TODO: Get var_run from ABRT configuration.
+  ABRT_SOCKET_PATH = '/var/run/abrt/abrt.socket'
 
   def self.handle_exception(exception)
     last_exception = $!
 
-    syslog.notice "detected unhandled Ruby exception in '#{$0}'"
+    report_error "detected unhandled Ruby exception in '#{$0}'", :notice
 
-    require 'abrt/exception.rb'
     last_exception.extend(ABRT::Exception)
 
     # TODO: Report only scripts with absolute path.
-
-    write_dump last_exception.format
+    write_dump(last_exception.format)
   end
 
 private
 
   def self.syslog
-    return @syslog if @syslog
-
-    require 'syslog'
-    @syslog = Syslog.open 'abrt'
+    @syslog ||= Syslog.open('abrt')
   end
 
-  def self.write_dump(backtrace)
-    begin
-      require 'socket'
+  def self.report_error(msg, facility=:err)
+    syslog.send :facility, msg
+  end
 
-      # TODO: Get var_run from ABRT configuration.
-      var_run = "/var/run"
-      socket = UNIXSocket.new "#{var_run}/abrt/abrt.socket"
-
+  def self.write_to_socket(backtrace)
+    UNIXSocket.open(ABRT_SOCKET_PATH) do |socket|
       socket.write "PUT / HTTP/1.1\r\n\r\n"
       socket.write "PID=#{Process.pid}\0"
       socket.write "EXECUTABLE=#{$PROGRAM_NAME}\0"
@@ -39,27 +38,24 @@ private
       socket.write "BASENAME=rbhook\0"
       socket.write "REASON=#{backtrace.first}\0"
       socket.write "BACKTRACE=#{backtrace}\0"
-      socket.close_write
+      yield socket.read
+    end
+  end
 
-      response = socket.read
+  def self.write_dump(backtrace)
+    unless File.exists(file) && File.socket?(file)
+      raise 'Cannot open connection to ABRT daemon (%s)' % ABRT_SOCKET_PATH
+    end
 
-      socket.close
+    write_to_socket(backtrace) do |response|
+      report_error('Empty response from the ABRT daemon') if response.empty?
 
       parts = response.split
-      code = Integer(parts[1]) rescue false
-      if (parts.size < 2) or
-        (not parts[0] =~ %r{^HTTP/}) or
-        (not code) or
-        (code >= 400)
-      then
-        if response.empty?
-          syslog.err "error sending data to ABRT daemon. Empty response received"
-        else
-          syslog.err "error sending data to ABRT daemon: #{response}"
-        end
+      code = parts[1].to_i rescue false
+
+      if (parts.size < 2) or (not parts[0] =~ /^HTTP/) or (not code) or (code >= 400)
+        syslog.err "error sending data to ABRT daemon: #{response}"
       end
-    rescue StandardError => e
-      syslog.err "can't communicate with ABRT daemon, is it running? #{e.message}"
     end
   end
 end
