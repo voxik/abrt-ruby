2d4aa28a7b1087b5cf3e4f8ef2ef4791fc52374a
Vít Ondruch <vondruch@redhat.com>
2012-07-04 13:08:31 +0200

    Refactoring.
    Based on comments by mifo at https://github.com/voxik/abrt-ruby/pull/1

--- lib/abrt/handler.rb
+++ lib/abrt/handler.rb
@@ -20,43 +20,49 @@ private
     @syslog ||= Syslog.open 'abrt'
   end
 
+  def self.report(backtrace, io = abrt_socket)
+    io.write "PUT / HTTP/1.1\r\n\r\n"
+    io.write "PID=#{Process.pid}\0"
+    io.write "EXECUTABLE=#{$PROGRAM_NAME}\0"
+    # TODO: Do we need specialized Ruby analyzer?
+    # io.write "ANALYZER=Ruby\0"
+    io.write "ANALYZER=Python\0"
+    io.write "BASENAME=rbhook\0"
+    io.write "REASON=#{backtrace.first}\0"
+    io.write "BACKTRACE=#{backtrace.join("\n")}\0"
+    io.close_write
+
+    yield io.read
+
+    io.close
+  rescue StandardError => e
+    syslog.err "can't communicate with ABRT daemon, is it running? #{e.message}"
+  end
+
   def self.write_dump(backtrace)
-    begin
-
-      # TODO: Get var_run from ABRT configuration.
-      var_run = "/var/run"
-      socket = UNIXSocket.new "#{var_run}/abrt/abrt.socket"
-
-      socket.write "PUT / HTTP/1.1\r\n\r\n"
-      socket.write "PID=#{Process.pid}\0"
-      socket.write "EXECUTABLE=#{$PROGRAM_NAME}\0"
-      # TODO: Do we need specialized Ruby analyzer?
-      # socket.write "ANALYZER=Ruby\0"
-      socket.write "ANALYZER=Python\0"
-      socket.write "BASENAME=rbhook\0"
-      socket.write "REASON=#{backtrace.first}\0"
-      socket.write "BACKTRACE=#{backtrace.join("\n")}\0"
-      socket.close_write
-
-      response = socket.read
-
-      socket.close
-
-      parts = response.split
-      code = Integer(parts[1]) rescue false
-      if (parts.size < 2) or
-        (not parts[0] =~ %r{^HTTP/}) or
-        (not code) or
-        (code >= 400)
-      then
-        if response.empty?
-          syslog.err "error sending data to ABRT daemon. Empty response received"
-        else
+    report backtrace do |response|
+      if response.empty?
+        syslog.err "error sending data to ABRT daemon. Empty response received"
+      else
+        parts = response.split
+        code = Integer(parts[1]) rescue false
+        if (parts.size < 2) or
+          (not parts[0] =~ %r{^HTTP/}) or
+          (not code) or
+          (code >= 400)
+        then
           syslog.err "error sending data to ABRT daemon: #{response}"
         end
       end
-    rescue StandardError => e
-      syslog.err "can't communicate with ABRT daemon, is it running? #{e.message}"
     end
   end
+
+  # TODO: Get VAR_RUN from ABRT configuration.
+  VAR_RUN = '/var/run'
+  ABRT_SOCKET_PATH = File.join VAR_RUN, 'abrt/abrt.socket'
+
+  def self.abrt_socket
+    UNIXSocket.new ABRT_SOCKET_PATH
+  end
+
 end
