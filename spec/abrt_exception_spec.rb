7310979c17975a75cf2f355f2fc9eafdf6626a71
Vít Ondruch <vondruch@redhat.com>
2012-07-17 12:26:35 +0200

    Add Exception#format spec.

---
+++ spec/abrt_exception_spec.rb
@@ -0,0 +1,28 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require 'abrt/exception'
+
+describe "ABRT::Exception" do
+  describe "#format" do
+    let(:exception) do
+      RuntimeError.new("baz").tap do |e|
+        e.set_backtrace([
+          "/foo/bar.rb:3:in `block in func'",
+          "/foo/bar.rb:2:in `each'",
+          "/foo/bar.rb:2:in `func'",
+          "/foo.rb:2:in `<main>'"
+        ])
+        e.extend(ABRT::Exception)
+      end
+    end
+
+    it "provides the formated exception message" do
+      exception.format.should == [
+        "/foo/bar.rb:3:in `block in func': baz (RuntimeError)",
+          "\tfrom /foo/bar.rb:2:in `each'",
+          "\tfrom /foo/bar.rb:2:in `func'",
+          "\tfrom /foo.rb:2:in `<main>'"
+      ]
+    end
+
+  end
+end
