06cbd9fa840be61ba18e741efbfddec854d7bb58
Vít Ondruch <vondruch@redhat.com>
2012-07-17 12:25:18 +0200

    Improve documentation.

--- lib/abrt/exception.rb
+++ lib/abrt/exception.rb
@@ -2,6 +2,8 @@
 
 module ABRT
   module Exception
+    # Provides the exception formated in the same way as Ruby does for standard
+    # error output.
     def format
       backtrace = self.backtrace.collect { |line| "\tfrom #{line}" }
       backtrace[0] = "#{self.backtrace.first}: #{self.message} (#{self.class.name})"
