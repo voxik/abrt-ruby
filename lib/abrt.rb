25d0bee514ebee7e6f1ccfab82225b6efccc4bd9
Vít Ondruch <vondruch@redhat.com>
2020-05-19 13:24:43 +0200

    Save the exeption prior doing anything else.
    It seems that `require` now changes value of `$!` due to some changes in
    RubyGems, therefore the exception is lost prior it gets processed.

--- lib/abrt.rb
+++ lib/abrt.rb
@@ -1,10 +1,12 @@
 at_exit do
+  exception = $!
+
   # Do not report every exception:
   #   SystemExit - raised by Kernel#exit call
   #   Interrupt - typically issued because the user pressed Ctrl+C
-  if $! and ![SystemExit, Interrupt].include?($!.class)
+  if exception and ![SystemExit, Interrupt].include?(exception.class)
     require File.join(File.dirname(__FILE__), 'abrt/handler')
-    ABRT.handle_exception($!)
+    ABRT.handle_exception(exception)
   end
 end
 
